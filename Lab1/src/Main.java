c626250f5a726e86d9310c2f9a4faa887e475bd4
Denisa <maria.denisa02@yahoo.com>
2023-02-21 18:45:10 +0200

    lab1

---
+++ Lab1/src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        String[] languages = {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
+        int n = (int) (Math.random() * 1_000_000);
+        n = n*3;
+        n=n+Integer.parseInt("10101", 2);
+        n=n+Integer.parseInt("FF", 16);
+        n=n*6;
+while(n>9)
+{
+    n=sum_cif(n);
+}
+int result=n;
+        System.out.println("Willy-nilly, this semester I will learn " + languages[result]);
+
+    }
+
+    static int sum_cif(int nr)
+    {
+        int s=0;
+        while(nr!=0)
+        {
+            s=s+nr%10;
+            nr=nr/10;
+        }
+        return s;
+    }
+
+
+}
